305e6435af6db06a20a0e5296b0a3b7876f511aa
Laffery <2387065420@qq.com>
2020-09-03 10:31:24 +0800

    init

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # requestbtn
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+This is a simple web page with only a button to send request for testing. You can code in [reqBtn](src/components/reqBtn.vue) and change axios callback function to do your test simply.
---
+++ src/components/reqBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <el-button style="width: 20%; margin-top: 40px" @click="Request" type="primary">Send Request</el-button>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      data: {}
+    }
+  },
+  methods: {
+    Request() {
+      let url = 'http://localhost:8088/test';
+      axios.post(url, this.data, {}).then(res => {
+        if(res.status === 200)
+          this.$message.success('Send request success!');
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+  }
+}
+</script>
